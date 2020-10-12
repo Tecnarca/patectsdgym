da5555f62649e941cf0a8d712349a43050d8eba8
Tecnarca <mail.tecnarca@gmail.com>
2020-10-12 12:39:54 +0200

    Added Implementation of all the NN and implemented GPU

--- tests/minitry.py
+++ tests/minitry.py
@@ -1,30 +1,54 @@
-from patectgan.preprocessing import GeneralTransformer
 from patectgan.pytorch_synthesizer import PytorchDPSynthesizer
-from patectgan.architectures.patectgan import PATECTGAN
+from patectgan.architectures import DPGAN, PATEGAN, DPCTGAN, PATECTGAN 
+from patectgan.preprocessing import GeneralTransformer
 
 import subprocess
 import os
 import string
 import pandas as pd
 
-git_root_dir = "C:\\Users\\Tecnarca\\Desktop\\dev-env\\service\\datasets"
+git_root_dir = subprocess.check_output("git rev-parse --show-toplevel".split(" ")).decode("utf-8").strip()
 
-csv_path = os.path.join(git_root_dir, os.path.join("PUMS.csv"))
+csv_path = os.path.join(git_root_dir, os.path.join("datasets/PUMS.csv"))
 
 df = pd.read_csv(csv_path)
 
-patectgan = PytorchDPSynthesizer(PATECTGAN(loss="wasserstein", regularization="dragan", verbose=True), None, epsilon=2)
+print("Starting PATEGAN...")
+pategan = PytorchDPSynthesizer(PATEGAN(), GeneralTransformer(), epsilon=1)
+pategan.fit(df, categorical_columns=['sex','educ','race','married'])
+synth_data = pategan.sample(df.size)
+s = synth_data.corr()
+d = df.corr()
+
+print("Starting DPGAN...")
+dpgan = PytorchDPSynthesizer(DPGAN(), GeneralTransformer(), epsilon=1)
+dpgan.fit(df, categorical_columns=['sex','educ','race','married'])
+synth_data = dpgan.sample(df.size)
+s = synth_data.corr()
+d = df.corr()
+
+print("Starting PATECTGAN...")
+patectgan = PytorchDPSynthesizer(PATECTGAN(loss="wasserstein", regularization="dragan", verbose=False), None, epsilon=1)
 patectgan.fit(df, categorical_columns=['sex','educ','race','married'])
 synth_data = patectgan.sample(df.size)
 s = synth_data.corr()
 d = df.corr()
-print(d.subtract(s))
-
-
-#from ctgan import CTGANSynthesizer
-#ctgan = CTGANSynthesizer()
-#ctgan.fit(df, ['sex','educ','race','married'], epochs=2000)
-#synth_data = ctgan.sample(df.size)
-#s = synth_data.corr()
-#d = df.corr()
 #print(d.subtract(s))
+
+print("Starting CTGAN...")
+from ctgan import CTGANSynthesizer
+ctgan = CTGANSynthesizer()
+ctgan.fit(df, ['sex','educ','race','married'], epochs=10)
+synth_data = ctgan.sample(df.size)
+s = synth_data.corr()
+d = df.corr()
+#print(d.subtract(s))
+
+print("Starting DPCTGAN...")
+dpctgan = PytorchDPSynthesizer(DPCTGAN(verbose=False), epsilon=1)
+dpctgan.fit(df, categorical_columns=['sex','educ','race','married'])
+synth_data = dpctgan.sample(df.size)
+s = synth_data.corr()
+d = df.corr()
+
+
